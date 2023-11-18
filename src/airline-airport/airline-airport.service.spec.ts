165a50a68f12cb9c507e815a3539e121867576fd
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-11-18 13:51:44 -0600

    Tests for airline-airport

--- src/airline-airport/airline-airport.service.spec.ts
+++ src/airline-airport/airline-airport.service.spec.ts
@@ -58,4 +58,56 @@ describe('AirlineAirportService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('addAirportToAirline should add an airport to an airline', async () => {
+    const newAirport: AirportEntity = await airportRepository.save({
+      id: faker.company.name(),
+      name: faker.company.name(),
+      code: faker.company.name(),
+      country: faker.company.name(),
+      city: faker.company.name(),
+      airlines: [],
+    });
+
+    const newAirline: AirlineEntity = await airlineRepository.save({
+      id: faker.company.name(),
+      name: faker.company.name(),
+      description: faker.lorem.sentence(),
+      foundationDate: faker.date.recent(),
+      webUrl: faker.image.url(),
+      airports: [],
+    });
+
+    const result: AirlineEntity = await service.addAirlineToAirport(
+      newAirline.id,
+      newAirport.id,
+    );
+
+    expect(result.airports.length).toBe(1);
+    expect(result.airports[0]).not.toBeNull();
+    expect(result.airports[0].name).toBe(newAirport.name);
+    expect(result.airports[0].code).toBe(newAirport.code);
+    expect(result.airports[0].country).toBe(newAirport.country);
+    expect(result.airports[0].city).toBe(newAirport.city);
+  });
+
+  it('findAirportsByAirline should return airports by airlines', async () => {
+    const storedAirport: AirportEntity[] = await service.findAirportsByAirline(
+      airline.id,
+    );
+    expect(storedAirport).not.toBeNull();
+  });
+
+  it('findAirportByAirline should return airports by airlines', async () => {
+    const airport: AirportEntity = airportsList[0];
+    const storedAirport: AirportEntity = await service.findAirportFromAirline(
+      airline.id,
+      airport.id,
+    );
+    expect(storedAirport).not.toBeNull();
+    expect(storedAirport.name).toBe(airport.name);
+    expect(storedAirport.code).toBe(airport.code);
+    expect(storedAirport.country).toBe(airport.country);
+    expect(storedAirport.city).toBe(airport.city);
+  });
 });
